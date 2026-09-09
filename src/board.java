2249ae4a45abdd55849e5f866ee68795e9aa3b3a
oikawatoru01 <julia.vitoria.nunes@escola.pr.gov.br>
2026-09-09 11:40:53 -0300

    Create board class with draw method

---
+++ src/board.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+public class board {
+
+    public void draw(Graphics20 g2) {
+        drawGrass(g2);
+    }
+    
+    private void drawGrass(Graphics20 g2) {
+        int title = GameConstamts.TITLE;
+        
+        for
+    }
+}
